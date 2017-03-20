1ce19d1ef962ab8dfd5df48f71d18135ddd602ac
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-20 19:10:34 +0400

    Builder core

---
+++ Creational Patterns/Builder/Builder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract void BuildPartC();
+        public abstract Product GetResult();
+    }
+
+    class ConcreteBuilder : Builder
+    {
+        Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("Part A");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("Part B");
+        }
+
+        public override void BuildPartC()
+        {
+            product.Add("Part C");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+}
---
+++ Creational Patterns/Builder/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace Builder
+{
+    class Product
+    {
+        ArrayList parts = new ArrayList();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            foreach (string part in parts)
+                Console.WriteLine(part);
+        }
+    }
+}
