2b4fbc720ec949e1ca083d0ff918b9ae3bc704b0
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-20 20:00:45 +0400

    Singletone

--- Creational Patterns/Singleton/Program.cs
+++ Creational Patterns/Singleton/Program.cs
@@ -10,6 +10,13 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+            Console.WriteLine(ReferenceEquals(instance1, instance2));
+
+            instance1.SingletonOperation();
+            string singletonData = instance1.GetSingletonData();
+            Console.WriteLine(singletonData);
         }
     }
 }
---
+++ Creational Patterns/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        static Singleton uniqueInstance;
+        string singletonData = string.Empty;
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new Singleton();
+
+            return uniqueInstance;
+        }
+
+        public void SingletonOperation()
+        {
+            singletonData = "SingletonData";
+        }
+
+        public string GetSingletonData()
+        {
+            return singletonData;
+        }
+    }
+}
