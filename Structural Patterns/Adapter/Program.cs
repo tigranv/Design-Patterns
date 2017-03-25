a19640e110276a5ea2ab30fc502d88185d89cca5
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-26 01:21:07 +0400

    finished

--- Structural Patterns/Adapter/Program.cs
+++ Structural Patterns/Adapter/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Adapter
+﻿namespace Adapter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Target target = new Adapter();
+            target.Request();
         }
     }
 }
