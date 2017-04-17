9a5557d5ab5e0c8e20b291346ad1d925597e9bf6
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-17 16:13:38 +0400

    concrete handler2

---
+++ Behavioral Patterns/Chain Of Responsibility/ConcreteHandler2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Of_Responsibility
+{
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request == 2)
+                Console.WriteLine("Two");
+            else if (Successor != null)
+                Successor.HandleRequest(request);
+        }
+    }
+}
