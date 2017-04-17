6f90c5d1b9d4795eeb7830bba2ba4c107ee22076
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-17 16:11:18 +0400

    abstract handler

---
+++ Behavioral Patterns/Chain Of Responsibility/Handler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Of_Responsibility
+{
+    abstract class Handler
+    {
+        public Handler Successor { get; set; }
+        public abstract void HandleRequest(int request);
+    }
+}
